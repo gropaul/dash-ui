9a45b925faadb40a4457ebd0850b054d16f2defa
PGross <paul.riedlingen@gmail.com>
2024-12-11 13:39:34 +0100

    changed to JsDelivr for DuckDB wasm

--- src/state/connections/duckdb-wasm.ts
+++ src/state/connections/duckdb-wasm.ts
@@ -119,31 +119,26 @@ export class DuckDBWasm implements DataConnection {
     }
 }
 
-
-const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
-    mvp: {
-        mainModule: 'duckdb-mvp.wasm',
-        mainWorker: 'duckdb/duckdb-browser-mvp.worker.js',
-    },
-    eh: {
-        mainModule: 'duckdb-eh.wasm',
-        mainWorker: 'duckdb/duckdb-browser-eh.worker.js',
-    },
-};
-
-
 async function staticDuckDBBundles(): Promise<{
     db: duckdb.AsyncDuckDB,
     connection: AsyncDuckDBConnection
 }> {
 
+    const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
+
     // Select a bundle based on browser checks
-    const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+    const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+
+    const worker_url = URL.createObjectURL(
+        new Blob([`importScripts("${bundle.mainWorker!}");`], {type: 'text/javascript'})
+    );
+
     // Instantiate the asynchronus version of DuckDB-wasm
-    const worker = new Worker(bundle.mainWorker!);
+    const worker = new Worker(worker_url);
     const logger = new duckdb.ConsoleLogger();
     const db = new duckdb.AsyncDuckDB(logger, worker);
     await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+    URL.revokeObjectURL(worker_url);
 
     const connection = await db.connect();
 
