bb6c475ebeb1927316539bfdc37d5eca95b02bed
PGross <paul.riedlingen@gmail.com>
2025-03-10 23:42:29 +0100

    added test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from "react";
+import {H1} from "@/components/ui/typography";
+
+export default function Test() {
+
+
+
+    return (
+        <>
+            <H1> Hoi! </H1>
+        </>
+    );
+}
