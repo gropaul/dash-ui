5a3add125dc13142e9d92e4374de18eaae960e90
PGross <paul.riedlingen@gmail.com>
2025-08-29 19:14:27 +0200

    removed md for now

--- src/state/connections/md-wasm/md-wasm-provider.ts
+++ src/state/connections/md-wasm/md-wasm-provider.ts
@@ -1,8 +1,36 @@
 import {RelationData} from "@/model/relation";
 import {Column} from "@/model/data-source-connection";
 import {duckDBTypeToValueType} from "@/model/value-type";
-import {MaterializedQueryResult, MDConnection, SpecialDuckDBValue} from "@motherduck/wasm-client";
+// import {MaterializedQueryResult, MDConnection, SpecialDuckDBValue} from "@motherduck/wasm-client";
 
+
+class MaterializedQueryResult {
+
+}
+
+class SpecialDuckDBValue {
+    toJS(): any {
+        return null;
+    }
+}
+
+class MDConnection {
+    static create(config: { mdToken: string }): MDConnection {
+        return new MDConnection();
+    }
+    async close(): Promise<void> {
+        // Close the connection
+    }
+
+    async isInitialized(): Promise<boolean> {
+        return true;
+    }
+
+    async evaluateQuery(query: string): Promise<MaterializedQueryResult> {
+        return new MaterializedQueryResult();
+    }
+
+}
 
 export interface MdWasmConfig {
     token: string;
@@ -110,40 +138,6 @@ export class MdWasmProvider {
 
         const token = this.config.token;
 
-        try {
-            const fakeToken = 'invalid'
-            const testConnection = MDConnection.create({
-                mdToken: fakeToken
-            });
-
-            const isInitialized = await testConnection.isInitialized();
-            console.log('Fake Token Initialized: ', isInitialized);
-            const versionResult = await testConnection.evaluateQuery("select version();");
-            const version = versionResult.data.singleValue();
-            console.log('Fake Token Motherduck version: ', version);
-        } catch (e) {
-            console.log('Fake token failed as expected. Error: ', (e as Error).message);
-        }
-
-        try {
-            // get from env var
-            const actualToken = process.env.NEXT_PUBLIC_MD_API_KEY;
-            if (!actualToken) {
-                throw new Error('No Motherduck token provided in NEXT_PUBLIC_MD_API_KEY');
-            }
-            const testConnection = MDConnection.create({
-                mdToken: actualToken
-            });
-            const isInitalized = await testConnection.isInitialized();
-            console.log('Real Token Initialized: ', isInitalized);
-            const versionResult = await testConnection.evaluateQuery("select version();");
-            const version = versionResult.data.singleValue();
-            console.log('Real Token Motherduck version: ', version);
-        } catch (e) {
-            console.log('Real token failed. Error: ', (e as Error).message);
-        }
-
-
         throw new Error('Not implemented yet');
 
 
@@ -166,32 +160,32 @@ export class MdWasmProvider {
 
 
 export function resultToRelationData(result: MaterializedQueryResult): RelationData {
-    const duckDBRows = result.data.toRows();
-
-    const rows = []
-    for (const ddBRow of duckDBRows) {
-        const rowJS: any[] = []
-        for (const col of result.data.columnNames()) {
-            let val = ddBRow[col];
-            if (val instanceof SpecialDuckDBValue) {
-                val = val.toJS();
-            }
-            rowJS.push(val);
-        }
-        rows.push(rowJS);
-    }
+    // const duckDBRows = result.data.toRows();
+    //
+    const rows: any[] = []
+    // for (const ddBRow of duckDBRows) {
+    //     const rowJS: any[] = []
+    //     for (const col of result.data.columnNames()) {
+    //         let val = ddBRow[col];
+    //         if (val instanceof SpecialDuckDBValue) {
+    //             val = val.toJS();
+    //         }
+    //         rowJS.push(val);
+    //     }
+    //     rows.push(rowJS);
+    // }
 
     const columns: Column[] = []
-    const numCols = result.data.columnCount
-    for (let colIndex = 0; colIndex < numCols; colIndex++) {
-        const colName = result.data.columnName(colIndex);
-        const colType = duckDBTypeToValueType(result.data.columnType(colIndex).name);
-        columns.push({
-            name: colName,
-            id: colName,
-            type: colType
-        });
-    }
+    // const numCols = result.data.columnCount
+    // for (let colIndex = 0; colIndex < numCols; colIndex++) {
+    //     const colName = result.data.columnName(colIndex);
+    //     const colType = duckDBTypeToValueType(result.data.columnType(colIndex).name);
+    //     columns.push({
+    //         name: colName,
+    //         id: colName,
+    //         type: colType
+    //     });
+    // }
 
     return {
         columns: columns,
