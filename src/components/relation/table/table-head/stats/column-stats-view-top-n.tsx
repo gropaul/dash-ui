f5285fd1dc9659a5f8e9c18080ed8c33f0217142
PGross <paul.riedlingen@gmail.com>
2025-11-29 23:02:30 +0100

    added selectable bar chart

--- src/components/relation/table/table-head/stats/column-stats-view-top-n.tsx
+++ src/components/relation/table/table-head/stats/column-stats-view-top-n.tsx
@@ -4,6 +4,8 @@ import dynamic from "next/dynamic";
 import {formatNumber} from "@/platform/number-utils";
 import {LerpColorHex} from "@/platform/colors-utils";
 import {useState} from "react";
+import {EChartsOption} from "echarts-for-react/src/types";
+import {EChartsInstance} from "echarts-for-react";
 
 const ReactECharts = dynamic(() => import("echarts-for-react"), {
     ssr: false,
@@ -17,38 +19,36 @@ interface TopNChartProps {
     className?: string;
 }
 
-export function ColumnStatsViewTopN({ topValues, othersCount, nonNullCount, className }: TopNChartProps) {
+function toggleSelection(value: any, values: any[]){
+    // if in, remove
+
+}
+
+export function ColumnStatsViewTopN({topValues, othersCount, nonNullCount, className}: TopNChartProps) {
     // Prepare data for horizontal bar chart - reversed to show max to min
     const data = [...topValues];
     if (othersCount && othersCount > 0) {
-        data.push({ value: 'Others', count: othersCount });
+        data.push({value: 'Others', count: othersCount});
     }
     // order the data by count ascending
     data.sort((a, b) => a.count - b.count);
 
-    const [selected, setSelected] = useState<any[]>([])
-
-    const onEvents = {
-        click: (params: any) => {
-            const index = params.dataIndex;
-
-            if (typeof index !== 'number') return;
-            const element = data[index];
-
-            if (selected.includes(element.value)) {
-                setSelected(selected.filter(v => v !== element.value));
-            } else {
-                setSelected([...selected, element.value]);
-            }
-        },
-    };
-
-
+    const [selected, setSelected] = useState<string[]>([]);
 
     const startColor = '#afc9ff';
     const endColor = '#e6eeff';
 
+    const categories = data.map(item => {
+        return item.value === null ? 'null' :
+            item.value === undefined ? 'undefined' :
+                String(item.value);
+    });
     const colors = data.map((item, index) => {
+
+        if (selected.includes(categories[index])){
+            return '#a6ffc2'
+        }
+
         if (data.length === 1) {
             return LerpColorHex(startColor, endColor, 0);
         }
@@ -61,14 +61,31 @@ export function ColumnStatsViewTopN({ topValues, othersCount, nonNullCount, clas
         if (item.value === null || item.value === 'null' || item.value === undefined || item.value === 'undefined') {
             return '#efefef';
         }
-        return LerpColorHex(startColor, endColor,  1 - t);
+        return LerpColorHex(startColor, endColor, 1 - t);
     });
 
-    const categories = data.map(item => {
-        return item.value === null ? 'null' :
-            item.value === undefined ? 'undefined' :
-                String(item.value);
-    });
+
+
+    function onChartReady(instance: EChartsInstance){
+        instance.on('click', (params: any) => {
+            const index = params.dataIndex;
+            console.log(index);
+            if (typeof index !== 'number') return;
+            const element = categories[index];
+
+            setSelected(prev => {
+                // Toggle logic
+                if (prev.includes(element)) {
+                    // remove
+                    return prev.filter(x => x !== element);
+                } else {
+                    // add
+                    return [...prev, element];
+                }
+            });
+        });
+    }
+
     const counts = data.map(item => item.count);
     const maxCount = Math.max(...counts);
 
@@ -114,11 +131,26 @@ export function ColumnStatsViewTopN({ topValues, othersCount, nonNullCount, clas
         series: [
             {
                 type: 'bar',
+                data: counts.map(() => maxCount),
+                yAxisIndex: 0,
+
+                // IMPORTANT: let ECharts determine the *full slot size* automatically
+                barWidth: '100%',      // fills entire row slot
+                barGap: '-100%',       // stack directly on top
+
+                itemStyle: {
+                    color: 'rgba(0,0,0,0)', // invisible
+                },
+                silent: false,
+                z: 0,
+            },
+            {
+                type: 'bar',
                 data: counts,
                 itemStyle: {
                     color: (params: any) => colors[params.dataIndex],
                     borderWidth: 1,
-                    borderRadius: [1,1, 1, 1],
+                    borderRadius: [1, 1, 1, 1],
                 },
                 label: {
                     show: true,
@@ -132,6 +164,7 @@ export function ColumnStatsViewTopN({ topValues, othersCount, nonNullCount, clas
                 },
                 barWidth: barHeight,
                 barCategoryGap: barGap,
+                z: 1,
             }
         ],
     };
@@ -155,11 +188,12 @@ export function ColumnStatsViewTopN({ topValues, othersCount, nonNullCount, clas
                     display: none; /* Chrome, Safari, Opera */
                 }
             `}</style>
+            {selected.length}
             <ReactECharts
                 option={option}
                 style={{ height: `${chartHeight}px`, width: '100%' }}
-                opts={{ renderer: 'svg' }}
-                onEvents={onEvents}
+                opts={{ renderer: 'canvas' }}
+                onChartReady={onChartReady}
             />
         </div>
     );
