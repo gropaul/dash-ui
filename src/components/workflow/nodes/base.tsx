4ac49c30a49b7c0ab7945a90e70591c2d8b2cf1e
PGross <paul.riedlingen@gmail.com>
2026-01-11 20:18:31 +0100

    base sizing problem fixed

--- src/components/workflow/nodes/base.tsx
+++ src/components/workflow/nodes/base.tsx
@@ -31,14 +31,15 @@ export function NodeBody(props: NodeBodyProps) {
     return (
         <div className="flex flex-col w-full h-full">
             <div
-                className="flex-1 rounded-md overflow-hidden"
+                className="flex-1 rounded-md overflow-visible w-full h-full"
                 style={{
                     padding: "1px", // Space for the gradient border
                     background: "#e4e4e4",
                     // boxShadow: "var(--node-shadow)"
                 }}
             >
-                <div 
+                <div
+                    className={`w-full h-full`}
                     style={{ 
                         backgroundColor: "#fafbfc",
                         borderRadius: "0.375rem", // Equivalent to rounded-md
@@ -48,7 +49,7 @@ export function NodeBody(props: NodeBodyProps) {
                         flexDirection: "column"
                     }}
                 >
-                    <div className={cn("flex-1",props.className)}>{children}</div>
+                    <div className={cn("flex-1 w-full h-full",props.className)}>{children}</div>
                 </div>
             </div>
         </div>
