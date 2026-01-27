15b1dcf0be84ac61c4b977438de683d7548035cb
PGross <paul.riedlingen@gmail.com>
2026-01-27 23:49:37 +0100

    fix: build

--- src/components/workflow/nodes/relation/index.tsx
+++
@@ -1,4 +0,0 @@
-export { RelationNode } from './relation-node';
-export { Toolbar } from './toolbar';
-export { ToolbarViewSwitcher } from './toolbar-view-switcher';
-export { ConditionalHandles } from './conditional-handles';
